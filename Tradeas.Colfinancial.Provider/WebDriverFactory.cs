e3bb6b3c539564d4cef3d4c522b3832a2f4ca56d
iamstupendous <vincent.megia@gmail.com>
2018-07-25 21:16:04 +0800

    adding logging to see if configuration is working

--- Tradeas.Colfinancial.Provider/WebDriverFactory.cs
+++ Tradeas.Colfinancial.Provider/WebDriverFactory.cs
@@ -1,19 +1,25 @@
 ﻿using System.IO;
 using System.Reflection;
+using log4net;
 using Microsoft.Extensions.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Tradeas.Colfinancial.Provider.Actors;
 
 namespace Tradeas.Colfinancial.Provider
 {
     public static class WebDriverFactory
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(BrokerActor));
+        
         public static IWebDriver Create(IConfiguration configuration)
         {
             var options = new ChromeOptions();
             options.AddArgument("--headless");
             options.AddArgument("-no-sandbox");
-            options.BinaryLocation = configuration["ChromeDriverPath"];
+            var path = configuration["ChromeDriverPath"];
+            Logger.Info($"setting chromedriver.exe path {path}");
+            options.BinaryLocation = path;
             var chromeDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             return new ChromeDriver(chromeDriverPath, options);
         }
